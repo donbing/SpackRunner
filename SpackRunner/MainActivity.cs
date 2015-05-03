e40f9434045df102222adfbf2ae67b03d6b32a9c
Chris Bingham <ccbing@gmail.com>
2015-05-03 14:26:26 +0100

    monogame spike to draw an animated sprite

---
+++ SpackRunner/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using Android.Views;
+
+namespace SpackRunner
+{
+	[Activity (Label = "SpackRunner"
+	, MainLauncher = true
+	, Icon = "@drawable/icon"
+	, AlwaysRetainTaskState = true
+	, LaunchMode = LaunchMode.SingleInstance
+	, ScreenOrientation = ScreenOrientation.SensorLandscape
+	, ConfigurationChanges = ConfigChanges.Orientation |
+	ConfigChanges.Keyboard |
+	ConfigChanges.KeyboardHidden)]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			var g = new MainGame ();
+			SetContentView ((View)g.Services.GetService (typeof(View)));
+			g.Run ();
+		}
+	}
+}
